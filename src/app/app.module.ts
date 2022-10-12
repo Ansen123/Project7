13f0d79129c11a8637f1d899a687a1e97b79c79e
Ansen123 <ansenfp@gmail.com>
2022-10-12 11:41:28 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TudoListComponent } from './tudo-list/tudo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TudoListComponent
   ],
   imports: [
     BrowserModule,
